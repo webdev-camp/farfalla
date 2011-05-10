6afa578cdceafe40903b83f8bf1f82ea05c9052d
Torsten Rüger <torsten@lightning.nu>
2011-05-10 11:11:44 +0300

    initial rails + farfalla-essentials version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farfalla::Application.config.secret_token = 'd9c34b74cb5ac6d3fff220e2fdbfbe804a997c800a444b1d0d79b4d98eade77ee1a6faa128375c6df3ddac1893202e814fa5175852463e4a475ece32249ca6e7'
