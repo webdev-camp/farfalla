e7c6e741a8bca17d68d3b74fd0be60a3212dbf39
Torsten Rüger <torsten@lightning.nu>
2011-08-29 19:11:35 +0300

    automated three list

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,7 +3,11 @@ class PageController < ApplicationController
   def index
     @page =  params[:page] || "index"
     response.headers['Content-type'] = 'text/html; charset=utf-8'
-    render :template =>  @page
+    if template_exists? @page
+      render :template =>  @page
+    else
+      render :template =>  "page/three"
+    end
   end
   
 
