0aed47ea3dd0454d9e22fbbc3f2dca3d14d0efc9
Torsten Rüger <torsten@lightning.nu>
2011-08-29 19:29:03 +0300

    move static files to own dir

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,10 +3,15 @@ class PageController < ApplicationController
   def index
     @page =  params[:page] || "index"
     response.headers['Content-type'] = 'text/html; charset=utf-8'
+    puts "PAGE static/#{@page}"
     if template_exists? @page
       render :template =>  @page
     else
-      render :template =>  "page/three"
+      if template_exists?("static/#{@page}")
+        render :template => "static/#{@page}"
+      else
+        render :template =>  "page/three"
+      end
     end
   end
   
