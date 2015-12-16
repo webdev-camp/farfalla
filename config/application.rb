4c50318f0c4ea8806bf19208e012389074e9a20b
Torsten Ruger <torsten@villataika.fi>
2015-12-16 21:12:47 +0200

    enable gc profiling

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require "susy"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+GC::Profiler.enable
+
 module Farfalla
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
