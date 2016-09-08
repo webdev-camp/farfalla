1371982e639a7d0665d73cf2d91c315d3e062e32
Torsten Ruger <torsten@villataika.fi>
2016-09-08 16:11:55 +0300

    creating pending test

---
+++ spec/factories/reseller.rb
@@ -0,0 +1,7 @@
+
+FactoryGirl.define do
+  factory :reseller do
+    name "Reseller"
+    post_code 10480
+  end
+end
--- spec/models/reseller_spec.rb
+++ spec/models/reseller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Reseller, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "factory is ok" do
+    res = build :reseller
+    expect(res.save).to be true
+  end
+
 end
