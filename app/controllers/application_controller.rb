133b37e596c6019abfb7ee388d167143a68a5c1f
Torsten Ruger <torsten@villataika.fi>
2016-09-30 11:41:51 +0300

    switch to locale en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :locale
+  before_filter :set_page
 
   protected
-  
-  def locale
-    I18n.locale = :fi
+
+  def set_page
     @page =  params[:page] || "index"
   end
 
--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Farfalla
 
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.use Rack::Attack
-    config.i18n.available_locales = :fi
-    config.i18n.default_locale = :fi
+    config.i18n.available_locales = :en
+    config.i18n.default_locale = :en
   end
 end
